57be0021ed4a744afe87ab461a00a71ca685d1da
Mudassar045 <mudassaralics007@gmail.com>
2019-11-01 19:49:53 +0500

    sort-utils: add sorYearMonths(kvArray: Arrray<string>) func

--- src/sort-utils.js
+++ src/sort-utils.js
@@ -4,7 +4,7 @@
  * return: sorted year months []
  */
 
-export const sortYearMonths = kvSet => {
+export const sortYearMonths = (kvSet: Set<string>) => {
 
     const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]	
     /* Sorting payments months names using index of sample array
@@ -13,6 +13,16 @@ export const sortYearMonths = kvSet => {
     */
     return Array.from(kvSet).sort((a, b) => months.indexOf(a.toLowerCase()) + 1 - months.indexOf(b.toLowerCase()) + 1)
     
+}
+
+export const sortYearMonths = (kvArray: Array<string>) => {
+    
+    const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]	
+    /* Sorting payments months names using index of sample array
+     * To avoid sort function negative result (0-1 => -1), added 1 so indexOf("january") 
+     * will be 0 to 1 and indexOf("jebruary") will be 1 to 2	
+    */
+    return kvArray.sort((a, b) => months.indexOf(a.toLowerCase()) + 1 - months.indexOf(b.toLowerCase()) + 1)
 }
 
 export default sortYearMonths
