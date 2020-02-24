948fefbc7b7e9e5751fae679a0fa2d1f475c92c6
Mudassar045 <mudassaralics007@gmail.com>
2020-02-24 22:57:21 +0500

    update README.md

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 ES6 utilities for personal usage
 
 - chunkify(kvArray = [], chunkSize = 10)
-- isDecimalNumber(number): boolean
+- isDecimalNumber(number: any): boolean
+- toTitleCase(text: string,  split_by: string): string
+
+[MIT-LICENSE](./LICENSE)
--- toTitleCase.js
+++ toTitleCase.js
@@ -1,12 +1,18 @@
-const toTitleCase = (text) => {
-    if(text == null || text === "") {
+/**
+ *  @params text: only string allow
+ *  @params split_by: Optional delimiter to split the string, has defaulter space separator
+ * 
+ *  Description: resturns title-cased text
+ */
+const toTitleCase = (text, split_by = " ") => {
+    if (text == null || text === "") {
         return ""
     }
     return text.trim()
         .toLowerCase()
-        .split(' ')
+        .split(split_by)
         .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-        .join(' ')
+        .join(split_by)
 }
 
 export default toTitleCase
